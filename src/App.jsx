64a528a5d95e8285b2ed24b639220ad15a5ff10c
Peter00010 <mwongela.oboe@gmail.com>
2024-02-13 08:25:02 +0300

    Resturucture

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import "./App.css";
-import Navbar from "./Navbar";
-import Carousel from "./Carousel";
 
     const App = () => {
 
+
     return (
+        
         <>
-        <>
-            <Navbar/>
-            <Carousel/>
-        </>
         </>
     );
 };
