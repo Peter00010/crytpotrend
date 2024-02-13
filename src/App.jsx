25ebed68d3c6f048d4bf625d9fd07339fa1574ae
Peter00010 <mwongela.oboe@gmail.com>
2024-02-13 07:16:44 +0300

    Navbar-First Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Navbar from "./Navbar";
+import Carousel from "./Carousel";
+
+    const App = () => {
+
+    return (
+        <>
+        <>
+            <Navbar/>
+            <Carousel/>
+        </>
+        </>
+    );
+};
+
+export default App;
